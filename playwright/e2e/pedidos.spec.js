d4489d7199d50dc1f15fd16518a065380eb163d4
Guilherme Trindade Almeida <gui_newyork@hotmail.com>
2026-01-23 23:08:36 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.js
+++ playwright/e2e/pedidos.spec.js
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test'
 
-test('Deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/')
+// AAA - Arrange, Act, Assert
 
-  // Checkpoint
+//Arrange
+ test('Deve consultar um pedido aprovado', async ({ page }) => {
+  await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' })).toBeVisible()
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-
   await expect(page.getByRole('heading', { name: 'Consultar Pedido' })).toBeVisible()
 
+  //Act
   await page.getByTestId('search-order-id').fill('VLO-U91KMJ')
-
   await page.getByTestId('search-order-button').click()
+
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-U91KMJ')
   await expect(page.getByTestId('order-result-status')).toBeVisible()
