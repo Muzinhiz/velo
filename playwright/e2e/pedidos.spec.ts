29bbbc7e9368dadf0be57fef6ca18cff06096c20
Guilherme Trindade Almeida <gui_newyork@hotmail.com>
2026-05-15 22:56:42 -0300

    Trocando de PO para Feature Actions

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,73 +1,65 @@
-import { test } from '@playwright/test'
-import { Navbar } from '../support/components/Navbar'
+import { test } from '../support/fixtures'
 import { generateOrderCode } from '../support/helpers'
-import { LandingPage } from '../support/pages/LandingPage'
-import { OrderLockupPage, type OrderDetails } from '../support/pages/OrderLockupPage'
+import type { OrderDetails } from '../support/actions/orderLookupActions'
 
 test.describe('Consulta de Pedido', () => {
-
-  let orderLockupPage: OrderLockupPage
-
-  test.beforeEach(async ({ page }) => {
-    await new LandingPage(page).goto()
-    await new Navbar(page).orderLockupLink()
-
-    orderLockupPage = new OrderLockupPage(page)
-    await new OrderLockupPage(page).validatePageLoaded()
+  test.beforeEach(async ({ app }) => {
+    await app.orderLookup.open()
   })
 
-  test('deve consultar um pedido aprovado', async ({ page }) => {
+  test('deve consultar um pedido aprovado', async ({ app }) => {
     const order: OrderDetails = {
       number: 'VLO-U91KMJ',
       status: 'APROVADO',
       color: 'Glacier Blue',
       wheels: 'aero Wheels',
       customer: { name: 'Guilherme Trindade', email: 'gui_newyork@hotmail.com' },
-      payment: 'À Vista'
+      payment: 'À Vista',
     }
-   
-    await orderLockupPage.searchOrder(order.number)
-    await orderLockupPage.assertOrderResultDetails(order)
+
+    await app.orderLookup.searchOrder(order.number)
+    await app.orderLookup.validateOrderDetails(order)
   })
 
-  test('deve consultar um pedido reprovado', async ({ page }) => {
+  test('deve consultar um pedido reprovado', async ({ app }) => {
     const order: OrderDetails = {
       number: 'VLO-LLDM4P',
       status: 'REPROVADO',
       color: 'Midnight Black',
       wheels: 'sport Wheels',
       customer: { name: 'Steve Jobs', email: 'jobs@apple.com' },
-      payment: 'À Vista'
+      payment: 'À Vista',
     }
-   
-    await orderLockupPage.searchOrder(order.number)
-    await orderLockupPage.assertOrderResultDetails(order)
+
+    await app.orderLookup.searchOrder(order.number)
+    await app.orderLookup.validateOrderDetails(order)
   })
 
-  test('deve consultar um pedido em analise', async ({ page }) => {
+  test('deve consultar um pedido em analise', async ({ app }) => {
     const order: OrderDetails = {
       number: 'VLO-DP4M46',
       status: 'EM_ANALISE',
       color: 'Lunar White',
       wheels: 'aero Wheels',
       customer: { name: 'Joao da Silva', email: 'joao@velo.dev' },
-      payment: 'À Vista'
+      payment: 'À Vista',
     }
-   
-    await orderLockupPage.searchOrder(order.number)
-    await orderLockupPage.assertOrderResultDetails(order)
+
+    await app.orderLookup.searchOrder(order.number)
+    await app.orderLookup.validateOrderDetails(order)
   })
 
-  test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
+  test('deve exibir mensagem quando o pedido não é encontrado', async ({ app }) => {
     const order = generateOrderCode()
 
-    await orderLockupPage.searchOrder(order)
-    await orderLockupPage.validateOrderNotFound()
+    await app.orderLookup.searchOrder(order)
+    await app.orderLookup.validateOrderNotFound()
   })
 
-  test('deve exibir mensagem quando o código está fora do padrão VLO-XXXXXX', async ({ page }) => {
-
-    await orderLockupPage.searchOrder('PED-SEM-VLO')
-    await orderLockupPage.validateOrderNotFound()
+  test('deve exibir mensagem quando o código está fora do padrão VLO-XXXXXX', async ({
+    app,
+  }) => {
+    await app.orderLookup.searchOrder('PED-SEM-VLO')
+    await app.orderLookup.validateOrderNotFound()
   })
 })
